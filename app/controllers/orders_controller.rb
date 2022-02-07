aa7d82aa62d36281f2b6047dee1b1d60ef772886
Kim Weible <kw00183@my.westga.edu>
2022-02-07 00:09:33 -0500

    [end chapter 12] chapter 12: add who_bought method to products_controller; create who_bought.atom.builder file in products view with atom feed code; add new has_many directive to product model; update routes in config with products resource; test the code with curl command after placing some new orders

--- app/controllers/orders_controller.rb
+++ app/controllers/orders_controller.rb
@@ -2,14 +2,16 @@ class OrdersController < ApplicationController
   include CurrentCart
   before_action :set_cart, only: [:new, :create]
   before_action :ensure_cart_isnt_empty, only: :new
-  before_action :set_order, only: %i[ show edit update destroy ]
+  before_action :set_order, only: [:show, :edit, :update, :destroy]
 
-  # GET /orders or /orders.json
+  # GET /orders
+  # GET /orders.json
   def index
     @orders = Order.all
   end
 
-  # GET /orders/1 or /orders/1.json
+  # GET /orders/1
+  # GET /orders/1.json
   def show
   end
 
@@ -22,7 +24,8 @@ class OrdersController < ApplicationController
   def edit
   end
 
-  # POST /orders or /orders.json
+  # POST /orders
+  # POST /orders.json
   def create
     @order = Order.new(order_params)
     @order.add_line_items_from_cart(@cart)
@@ -43,25 +46,26 @@ class OrdersController < ApplicationController
     end
   end
 
-  # PATCH/PUT /orders/1 or /orders/1.json
+  # PATCH/PUT /orders/1
+  # PATCH/PUT /orders/1.json
   def update
     respond_to do |format|
       if @order.update(order_params)
-        format.html { redirect_to order_url(@order), notice: "Order was successfully updated." }
+        format.html { redirect_to @order, notice: 'Order was successfully updated.' }
         format.json { render :show, status: :ok, location: @order }
       else
-        format.html { render :edit, status: :unprocessable_entity }
+        format.html { render :edit }
         format.json { render json: @order.errors, status: :unprocessable_entity }
       end
     end
   end
 
-  # DELETE /orders/1 or /orders/1.json
+  # DELETE /orders/1
+  # DELETE /orders/1.json
   def destroy
     @order.destroy
-
     respond_to do |format|
-      format.html { redirect_to orders_url, notice: "Order was successfully destroyed." }
+      format.html { redirect_to orders_url, notice: 'Order was successfully destroyed.' }
       format.json { head :no_content }
     end
   end
@@ -76,11 +80,12 @@ class OrdersController < ApplicationController
     def order_params
       params.require(:order).permit(:name, :address, :email, :pay_type)
     end
+  #...
 
-    private
-       def ensure_cart_isnt_empty
-         if @cart.line_items.empty?
-           redirect_to store_index_url, notice: 'Your cart is empty'
-         end
+  private
+     def ensure_cart_isnt_empty
+       if @cart.line_items.empty?
+         redirect_to store_index_url, notice: 'Your cart is empty'
        end
+     end
 end

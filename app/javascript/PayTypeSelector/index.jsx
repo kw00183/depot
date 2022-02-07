b6df708bafd701752a28b9249313640cb846bcf4
Kim Weible <kw00183@my.westga.edu>
2022-02-07 00:43:45 -0500

    chapter 13: create new pay_type javascript pack file; create PayTypeSelector folder in javascript directory and add index.jsx file; update javascript_pack_tag value in new html orders; update _form.html.erb in orders with new div; update dropdown select in PayTypeSelector index with onChange; add onPayTypeSelected event to PayTypeSelector index

---
+++ app/javascript/PayTypeSelector/index.jsx
@@ -0,0 +1,23 @@
+import React from 'react'
+
+class PayTypeSelector extends React.Component {
+  onPayTypeSelected(event) {
+    console.log(event.target.value);
+  }
+
+  render() {
+    return (
+      <div className="field">
+        <label htmlFor="order_pay_type">Pay type</label>
+        <select id="order_pay_type" onChange={this.onPayTypeSelected}
+            name="order[pay_type]">
+          <option value="">Select a payment method</option>
+          <option value="Check">Check</option>
+          <option value="Credit card">Credit card</option>
+          <option value="Purchase order">Purchase order</option>
+        </select>
+      </div>
+    );
+  }
+}
+export default PayTypeSelector

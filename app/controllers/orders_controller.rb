0f8a4d22ac1b4f90fec67058878efb3ae0bac171
Kim Weible <kw00183@my.westga.edu>
2022-02-06 21:54:58 -0500

    chapter 12: add button for checkout and modify code for empty cart button in _cart.html.erb; update orders_controller with include and before_action directives with private method to ensure cart is not empty; add in requires item in cart test and update should get new test in orders_controller_test

--- app/controllers/orders_controller.rb
+++ app/controllers/orders_controller.rb
@@ -1,4 +1,7 @@
 class OrdersController < ApplicationController
+  include CurrentCart
+  before_action :set_cart, only: [:new, :create]
+  before_action :ensure_cart_isnt_empty, only: :new
   before_action :set_order, only: %i[ show edit update destroy ]
 
   # GET /orders or /orders.json
@@ -67,4 +70,11 @@ class OrdersController < ApplicationController
     def order_params
       params.require(:order).permit(:name, :address, :email, :pay_type)
     end
+
+    private
+       def ensure_cart_isnt_empty
+         if @cart.line_items.empty?
+           redirect_to store_index_url, notice: 'Your cart is empty'
+         end
+       end
 end

d8e9fbe25f4e80dc19adccf360de78fc70dd49e0
Kim Weible <kw00183@my.westga.edu>
2022-02-06 21:14:57 -0500

    [end chapter 11] chapter 11: generate channel products; update subscribed method in products_channel to stream_from products; add config line to development environment; update update method in products_controller to use action cable; update index method in products_controller; add in logic to received(data) to use querySelector in products_channel

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -8,6 +8,9 @@ Rails.application.configure do
   # since you don't have to restart the web server when you make code changes.
   config.cache_classes = false
 
+  # disable access from remote localhost
+  config.action_cable.disable_request_forgery_protection = true
+
   # Do not eager load code on boot.
   config.eager_load = false
 
